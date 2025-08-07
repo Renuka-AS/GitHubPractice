da79c50ed7648a0c87d616d4c41f4945fbc32470
Renu <renu012379k@gmail.com>
2025-08-07 13:18:50 +0530

    added another lines

--- src/practiceCode/Test1.java
+++ src/practiceCode/Test1.java
@@ -4,5 +4,6 @@ public class Test1 {
 public static void main(String[] args) {
 	System.out.println("This is my first cloned repository ");
 	System.out.println("this is my first trail of cloning in another GitHub account");
+	System.out.println("added another line to my code");
 }
 }
