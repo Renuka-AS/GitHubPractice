06adf89c8237e00fdf68aee93023ff82cf32e607
Renu <renu012379k@gmail.com>
2025-08-07 12:36:13 +0530

    commiting test1 to brach1

---
+++ src/practiceCode/Test1.java
@@ -0,0 +1,8 @@
+package practiceCode;
+
+public class Test1 {
+public static void main(String[] args) {
+	System.out.println("This is my first cloned repository ");
+	System.out.println("this is my first trail of cloning in another GitHub account");
+}
+}
