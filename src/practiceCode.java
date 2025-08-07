b9a459c157027fa35dd04e190a46be1be987ee36
Renu <renu012379k@gmail.com>
2025-08-07 10:06:29 +0530

    First Commit

---
+++ src/practiceCode.java
@@ -0,0 +1,9 @@
+
+public class practiceCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hey I am exploring GitHub process through eclipse");
+	}
+
+}
